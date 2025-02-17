32f96654ffe9d5c8f55ac06c9d13757f0ef83e26
SerJLemas <maksiomRpp@gmail.com>
2025-02-17 23:15:14 +0300

    Initial commit

---
+++ src/main/java/org/example/airqualitysvc/AirQualitySvcApplication.java
@@ -0,0 +1,13 @@
+package org.example.airqualitysvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirQualitySvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirQualitySvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/airqualitysvc/AirQualitySvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.airqualitysvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirQualitySvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
