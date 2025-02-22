c28bb33f633b78d4ed321974d59b84378859688c
malexj <alexmaximovj@gmail.com>
2025-02-23 01:25:37 +0200

    Fix test

--- src/test/java/com/serjlemast/RunQualityApplicationTests.java
+++ src/test/java/com/serjlemast/RunQualityApplicationTests.java
@@ -1,9 +1,13 @@
 package com.serjlemast;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.amqp.rabbit.test.context.SpringRabbitTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig
+@SpringRabbitTest
 class RunQualityApplicationTests {
 
     @Test
