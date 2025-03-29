e3d8cbcd307bb8d1d752c1b962b118f636f14904
malexj <alexmaximovj@gmail.com>
2025-03-29 22:25:49 +0200

    Add angular js 4

--- src/main/java/com/serjlemast/configuration/WebConfig.java
+++ src/main/java/com/serjlemast/configuration/WebConfig.java
@@ -1,17 +1,14 @@
 package com.serjlemast.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    // Разрешаем доступ к файлам в папке static
     registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
   }
 }
---
+++ src/main/java/com/serjlemast/controller/PingApiController.java
@@ -0,0 +1,28 @@
+package com.serjlemast.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/ping")
+@RequiredArgsConstructor
+public class PingApiController {
+
+  @RequestMapping(method = RequestMethod.HEAD)
+  public ResponseEntity<Void> pong() {
+    log.debug("ping pong: HEAD");
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping
+  public ResponseEntity<Void> pongGet() {
+    log.debug("ping pong: GET");
+    return ResponseEntity.ok().build();
+  }
+}
