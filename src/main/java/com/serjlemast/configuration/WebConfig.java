d05d89cd81b8aca3d9f5f2efea53812e67f82c71
malexj <alexmaximovj@gmail.com>
2025-03-29 22:59:49 +0200

    Add angular js 7

--- src/main/java/com/serjlemast/configuration/WebConfig.java
+++ src/main/java/com/serjlemast/configuration/WebConfig.java
@@ -6,9 +6,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    registry.addResourceHandler("/js/**")
+            .addResourceLocations("classpath:/static/js/");
   }
 }
