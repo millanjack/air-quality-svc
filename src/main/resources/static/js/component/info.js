6ef0abe954793b81d6da07182c02f6d29a48e5f8
malexj <alexmaximovj@gmail.com>
2025-05-06 19:26:27 +0300

    Fix UI

--- src/main/resources/static/js/component/info.js
+++ src/main/resources/static/js/component/info.js
@@ -20,38 +20,48 @@ app.component('info', {
                </p>
                <br>
                <h6>Core Components of an IoT System:</h6>
+               <br>
                <b> 1. Sensors:</b>
                <br>
-               Collect real-world data (e.g., temperature, humidity, motion)<br>
+               Collect real-world data (e.g., temperature, humidity, air quality)
                <br>
                <br>
-               <b>2. Controller Devices:</b><br><br>
-               Controller: Microcontrollers or edge computing devices that process sensor data and communicate with the cloud or other systems<br>
-               Functions: Read sensor data, process or transform it (optional), and send it to a server<br>
-               Programming: Controllers run code written in languages like Python, java, etc.<br>
+               <b>2. Controller Devices:</b>
+               <br>
+               Microcontrollers computing devices that process sensor data and communicate with the cloud or other systems<br>
+               Read sensor data, process and send it to a backend server<br>
+               Controllers run code written in languages like Python, Java, etc.
                <br>
                <br>
-               <b>3. Connectivity:</b><br>
+               <b>3. Connectivity:</b>
                <br>
-               Network Protocols: Ensures communication between devices, controllers, and the cloud<br>
-               Data Transfer: Sends data from the controller to the cloud or backend system for processing and storage<br>
+               Ensures communication between devices, controllers, and the cloud<br>
+               Sends data from the controller to the backend system for processing and storage
                <br>
                <br>
-               <b>4. Cloud/Backend System:</b><br>
-               Backend Server: A system that receives data from edge devices, processes it, stores it, and might trigger actions based on it<br>
-               Databases: Stores sensor data for future analysis and access<br>
-               <br><br>
-               <b>5. Data Storage:</b><br>
+               <b>4. Cloud/Backend System:</b>
+               <br>
+               A system that receives data from controllers/devices, processes it, stores it<br>
+               Stores sensor data for future analysis and access
+               <br>
+               <br>
+               <b>5. Data Storage:</b>
+               <br>
                Databases: Stores incoming data for long-term storage and easy retrieval<br>
-               Function: Ensures the data is stored and accessible for analysis or reporting<br>
-               <br><br>
-               <b>6. Processing & Analytics:</b><br>
-               Data Analysis: Processes the collected data, often involving machine learning or analytics to gain insights or trigger actions<br>
-               <br><br>
-               <b>7. User Interface (Dashboard):</b><br>
-               Visualization & Control: Provides a graphical interface to monitor data or interact with the system<br>
-               Function: Users can view sensor data in real-time, analyze trends, or manually control devices<br>
-               <br><br>
+               Function: Ensures the data is stored and accessible for analysis or reporting
+               <br>
+               <br>
+               <b>6. Processing & Analytics:</b>
+               <br>
+               Processes the collected data, often involving machine learning or analytics to gain insights or trigger actions
+               <br>
+               <br>
+               <b>7. User Interface (Dashboard):</b>
+               <br>
+               Provides a graphical interface to monitor data or interact with the system<br>
+               Users can view sensor data in real-time, analyze trends, or manually control devices
+               <br>
+               <br>
             </div>
             <div class="card-footer text-muted"><br></div>
          </div>
