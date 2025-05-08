2eb6d068eb24341fd8f01363ea951504c55c3697
malexj <alexmaximovj@gmail.com>
2025-05-08 21:20:05 +0300

    enable CORS

--- src/main/java/com/serjlemast/controller/page/PageViewController.java
+++ src/main/java/com/serjlemast/controller/page/PageViewController.java
@@ -1,9 +1,11 @@
 package com.serjlemast.controller.page;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@CrossOrigin(origins = "*")
 @Controller
 public class PageViewController {
 
--- src/main/java/com/serjlemast/controller/statistics/StatisticsRestController.java
+++ src/main/java/com/serjlemast/controller/statistics/StatisticsRestController.java
@@ -4,13 +4,13 @@ import com.serjlemast.controller.statistics.dto.SensorResponse;
 import com.serjlemast.service.TelemetryService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/api")
 @RequiredArgsConstructor
